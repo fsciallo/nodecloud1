41591f89e854ebb65039c822cc7e8e7d8dafa27c
fsciallo <fsciallo@dtcc.edu>
2017-02-13 12:30:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(req, res) {
 	res.end();
 });
 
-var port = 10001;
+var port = 3500;
 server.listen(port, function() {
 	console.log('server listenting on port '+ port);
 });
